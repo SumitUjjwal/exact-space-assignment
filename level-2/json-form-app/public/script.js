6d2c122ca000ca2c710d8f835ad54cd46173c786
SumitUjjwal <sumitonboard@gmail.com>
2023-09-02 19:13:07 +0530

    rendering json data in formatted way

--- level-2/json-form-app/public/script.js
+++ level-2/json-form-app/public/script.js
@@ -1,15 +1,16 @@
-let container = "";
-const form = document.getElementById("form");
+let container = ""; // Initialize a variable to store response data
+const form = document.getElementById("form"); // Get the form element by its ID
 
+// Add a submit event listener to the form
 form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const inputJsonData = document.getElementById("inputJsonData").value;
+    e.preventDefault(); // Prevent the default form submission behavior
+    const inputJsonData = document.getElementById("inputJsonData").value; // Get the JSON data from the textarea
 
     try {
-        const inputParseData = JSON.parse(inputJsonData);
-        console.log(inputParseData);
+        const inputParseData = JSON.parse(inputJsonData); // Parse the JSON data
+        // console.log(inputParseData);
 
-        // POST the parsed data
+        // POST the parsed data to the server
         const request = await fetch("/post", {
             method: "POST",
             headers: {
@@ -17,42 +18,41 @@ form.addEventListener("submit", async (e) => {
             },
             body: JSON.stringify(inputParseData)
         });
-        const response = await request.json();
-        container = response;
 
-        // Call the 'render' function to display the response
+        const response = await request.json(); // Parse the response as JSON
+        container = response; // Store the response data in the 'container' variable
+
+        // Call the 'render' function to display the response data
         render(container);
     } catch (error) {
-        console.log(error);
-        alert("Invalid! Please write valid JSON data.");
+        // console.log(error);
+        alert("Invalid JSON format! Please enter valid JSON data."); // Display an alert for invalid JSON data
     }
 });
 
-
 // Rendering the input data in the 'formContainer'
 function render(data) {
-    const formContainer = document.getElementById("formContainer");
-    formContainer.style.display = "block";
-    formContainer.innerHTML = "";
+    const formContainer = document.getElementById("formContainer"); // Get the form container element
+    formContainer.style.display = "block"; // Make the form container visible
+    formContainer.innerHTML = ""; // Clear any previous content
 
-    const form = document.createElement("form");
+    const form = document.createElement("form"); // Create a new form element
     for (let key in data) {
-        const label = document.createElement("label");
+        const label = document.createElement("label"); // Create a label for each key
         label.innerText = key;
         label.setAttribute("class", "output-label");
 
-        const input = document.createElement("input");
+        const input = document.createElement("input"); // Create an input field for each value
         input.type = "text";
         input.name = key;
         input.value = data[key];
         input.readOnly = true;
         input.setAttribute("class", "output-input");
 
-        const lineBreak = document.createElement("br");
+        const lineBreak = document.createElement("br"); // Create a line break
 
-        form.append(label, input, lineBreak);
+        form.append(label, input, lineBreak); // Append the label, input, and line break to the form
     }
 
-    formContainer.append(form);
+    formContainer.append(form); // Append the form to the form container
 }
-
